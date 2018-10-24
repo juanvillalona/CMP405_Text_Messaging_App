9d7d2bd951109081a341987ad53a700f2c02a321
juan.villalona@lc.cuny.edu <juan.villalona@lc.cuny.edu>
2018-10-24 14:35:21 -0400

    Added a JFrame and a text area

--- src/Driver.java
+++ src/Driver.java
@@ -1,22 +1,36 @@
+import java.awt.BorderLayout;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.concurrent.TimeUnit;
 
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
 public class Driver {
 
 	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame("James/Yunaelin/Fayra");
+		frame.setSize(250, 250);
+		frame.setLocation(300, 300);
+		final JTextArea textArea = new JTextArea(10,40);
+		frame.getContentPane().add(BorderLayout.CENTER, textArea);
+		
+		frame.setVisible(true);
+		
+		
 		Socket socket = new Socket(64000);
 		
 		InetAddress myAddress = null;
 		//byte[] teacherAddress = new byte[]{192, 168, 1, 115};
-		InetAddress teacherAddress = null;
-		try {
-			teacherAddress = InetAddress.getByName("192.168.1.115");
-		} catch (UnknownHostException e1) {
-			e1.printStackTrace();
-		}
+		//InetAddress teacherAddress = null;
+//		try {
+//			//teacherAddress = InetAddress.getByName("192.168.1.115");
+//		} catch (UnknownHostException e1) {
+//			e1.printStackTrace();
+//		}
 		
 		try {
 			myAddress = InetAddress.getLocalHost();
@@ -31,9 +45,10 @@ public class Driver {
 		socket.send("I am having fun writing code", myAddress, 64000);
 		socket.send("What are you doing?", myAddress, 64000);
 		socket.send("Are you having fun?", myAddress, 64000);
+		socket.send("im good", myAddress, 64000);
 		
-		socket.send("juanV says OLA!", teacherAddress, 64000);
-		System.out.println(teacherAddress);
+		//socket.send("juanV says OLA!", teacherAddress, 64000);
+		//System.out.println(teacherAddress);
 		try {
 			System.out.println("Main is sleeping");
 			TimeUnit.SECONDS.sleep(5);
@@ -49,7 +64,7 @@ public class Driver {
 			packet = socket.receive();
 			if (packet != null) {
 				String message = new String(packet.getData());
-				System.out.println("Message = " + message);
+				textArea.append("Message = " + message + "\n");
 			}
 		} while(packet != null);
 		
